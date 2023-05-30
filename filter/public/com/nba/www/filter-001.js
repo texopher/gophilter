b39f9abb2cb8969a01e8753d777f286c19f33826
texopher - Textify World Wide Web <134022017+texopher@users.noreply.github.com>
2023-05-30 18:40:32 -0400

    Update filter-001.js

--- filter/public/com/nba/www/filter-001.js
+++ filter/public/com/nba/www/filter-001.js
@@ -1,12 +1,20 @@
 function __exec__(data) {
   data.output(data.input());
-  var nodes = get_tags_class_start(data, 'div', 'PlayerSummary_mainInnerBio__');
-  var text = '';
-  if (nodes.size() > 0) {
-    text = nodes.get(0).text(); 
-  }
+  var player_name = get_player_name(data);
+  var text = player_name;
   var item = new com.github.progrocus.seventy.core.Gopher.Item("i", "  ------------- HARD TO READ: " + text + " ----------------");
   data.input().get("goDoc").response.add(item);
+}
+
+function get_player_name(data) {
+  var target = '';
+  var nodes = get_tags_class_start(data, 'div', 'PlayerSummary_mainInnerBio__');
+  if (nodes.size() > 0) {
+    if (nodes.get(0).children().size() >= 3) {
+      target = nodes.get(0).children().get(1).text() + ' ' + nodes.get(0).children().get(2).text();
+    }
+  }
+  return target;
 }
 
 function get_tags_class_start(data, tagname, class_start) {
