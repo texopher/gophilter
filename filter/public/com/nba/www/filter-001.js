153b987a9af401976490861190b524a08af946da
texopher - Textify World Wide Web <134022017+texopher@users.noreply.github.com>
2023-05-30 18:06:10 -0400

    Update filter-001.js

--- filter/public/com/nba/www/filter-001.js
+++ filter/public/com/nba/www/filter-001.js
@@ -1,4 +1,4 @@
 function __exec__(data) {
   var item = new com.github.progrocus.seventy.core.Gopher.Item("i", "  ------------- HARD TO READ ----------------");
-  data.input().get("doc").response.add(item);
+  data.input().get("goDoc").response.add(item);
 }
