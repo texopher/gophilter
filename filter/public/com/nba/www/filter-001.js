168b99897ba3e9391bcad3bc9d8632262bba558d
texopher - Textify World Wide Web <134022017+texopher@users.noreply.github.com>
2023-05-30 19:46:10 -0400

    Update filter-001.js

--- filter/public/com/nba/www/filter-001.js
+++ filter/public/com/nba/www/filter-001.js
@@ -2,9 +2,32 @@ function __exec__(data) {
   data.output(data.input());
   var player_name = get_player_name(data);
   write_text(data, "=========================================");
-  write_text(data, "PLAYER: " + player_name);
+  write_text(data, pad_head("PLAYER: " + player_name));
   write_text(data, "  -------------------------------------");
+  write_text(data, "");
 
+}
+
+function intPart(text) {
+  var idx = lang().newString(text).indexOf('.');
+  if (idx >= 0) {
+    return parseInt(lang().newString(text).substring(0, idx));
+  }
+  return parseInt(text);
+}
+
+function pad_head(text) {
+  var head = lang().newString("=========================================");
+  var left = intPart('' + ((head.length() - lang().newString(text).length())/2));
+  if (left < 0) {
+    left = 0;
+  }
+  var target = '';
+  for (var i = 0; i < left; i++) {
+    target = target + ' ';
+  }
+  target = target + text;
+  return target;
 }
 
 function write_text(data, text) {
